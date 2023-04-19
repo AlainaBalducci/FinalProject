9d9a02a84b2b19660b5ab5d27c3c76fc609dfd0f
Alaina Balducci <alainabalducci@gmail.com>
2023-04-19 11:02:12 -0500

    added some IAction skeleton methods in controllers and added view folders

---
+++ Kapow/Controllers/FoodTagController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kapow.Controllers
+{
+    public class FoodTagController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Kapow/Models/Profile.cs
+++ Kapow/Models/Profile.cs
@@ -7,17 +7,17 @@ namespace Kapow.Models
         public int Id { get; set; }
         public string? UserName { get; set; }
         public string? FirstName { get; set; }
-        public string? Email { get; set; }
+        //public string? Email { get; set; }
         public string? Location { get; set; }
         public List<Restaurant>? Restaurants { get; set; }
         public ICollection<FoodTag>? FoodTags { get; set; }
 
-        public Profile(int id, string? userName, string? firstName, string? email, string? location)
+        public Profile(int id, string? userName, string? firstName, string? location)
         {
             Id = id;
             UserName = userName;
             FirstName = firstName;
-            Email = email;
+            //Email = email;
             Location = location;
             Restaurants = new List <Restaurant>();
             FoodTags = new List<FoodTag>();
